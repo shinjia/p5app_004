e83db6363fa9054bbd5f4f1ec27724243bc0929c
Shinjia <shinjia168@gmail.com>
2018-09-29 03:27:45 +0800

    update

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,9 +1,9 @@
  function setup()
 {
- 	createCanvas(displayWidth, displayHeight);
+  createCanvas(displayWidth, displayHeight);
   background(220, 220, 160);
-	strokeWeight(10)
-	stroke(0);
+  strokeWeight(8)
+  stroke(0);
 }
 
 function touchMoved()
