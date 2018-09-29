e08766220bbb66f2d42d83f78308ae1b01f45260
Shinjia <shinjia168@gmail.com>
2018-09-29 12:27:17 +0800

    v0.2.1 update

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,8 +1,8 @@
  function setup()
 {
- 	createCanvas(displayWidth, displayHeight);
-  background(220, 220, 160);
-	strokeWeight(10)
+ 	createCanvas(windowWidth, windowHeight);
+    background(220, 220, 160);
+	strokeWeight(8)
 	stroke(0);
 }
 
